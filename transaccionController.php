9a6edea9a234a39b0eaeca2a96d52e9aaad41790
Karen <41383218+nathy26@users.noreply.github.com>
2019-10-23 17:26:58 -0500

    Create transaccionController.php

---
+++ transaccionController.php
@@ -0,0 +1,20 @@
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use \Illuminate\Support\Facades\Input;
+
+/**
+ * Description of TransaccionController
+ *
+ * @author johnney
+ */
+class TransaccionController extends Controller{
+    
+    public function listart($external) {
+        
+        $cuenta = \App\Models\Cuenta::where('external_id', $external)->first();
+        $cuenta = \App\Models\Cuenta::find($cuenta->id);
+            
+        $dato['listatc'] = \App\Models\Transaccion::all();
+        return view("fragmentos.cuenta.cuenta", $datos);
+    }
+}
